ec32699517f46f6ac5573e8ed1c446ee63a507f2
lukas2005 <lukas2005.38@gmail.com>
2018-05-14 18:19:11 +0200

    changed the PM icon to `chat`  trying to get tooltips working

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -87,6 +87,11 @@ let vue = new Vue({
 				id: 1,
 				name: "N-O-D-E Community",
 				img: "https://cdn.discordapp.com/icons/438687916026232842/18c1e0ed2eda1b3e88a0515e28b3b50f.png"
+			},
+			{
+				id: 2,
+				name: "Valkyrie",
+				img: "https://cdn.discordapp.com/attachments/373141391909584910/445586709992833034/image.png"
 			}
 		],
 		friendsOnline: 10
@@ -104,4 +109,10 @@ function main() {
 	// showDialog("Hello", () => {
 	// 	console.log("Yay");
 	// });
+	$(document).ready(function(){
+		$("[data-toggle=\"tooltip\"]").tooltip();   
+	});
+	$(".guild").hover((e) => {
+		$(e.target).tooltip();
+	});
 }
