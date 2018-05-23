3e858db0dadac7ef2d5e87955d884afc263385ff
lukas2005 <lukas2005.38@gmail.com>
2018-05-23 20:51:07 +0200

    forgot to checkout back to master after deploying the preview

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -8,3 +8,4 @@ mv -v ./preview/* ./$TRAVIS_BANCH/$TRAVIS_COMMIT/
 git add .
 git commit -a -m "Built and uploaded preview for commit $TRAVIS_COMMIT on branch $TRAVIS_BRANCH"
 git push
+git checkout master
