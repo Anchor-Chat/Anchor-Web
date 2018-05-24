e65e2c10a28f79c508eeb778eb142e8956ae431b
lukas2005 <lukas2005.38@gmail.com>
2018-05-24 14:07:20 +0200

    Trying to now just set up a repo in a separate dir to upload to gh-pages

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -1,7 +1,12 @@
 set -e
 
+echo Init...
+mkdir tmp_gh_deploy
+cd tmp_gh_deploy
+git init
+
 echo Logging into git
-git remote set-url origin https://lukas2005:${GH_TOKEN}@github.com/Anchor-Chat/Anchor-Web.git
+git remote add origin https://lukas2005:$GH_TOKEN@github.com/Anchor-Chat/Anchor-Web.git
 
 echo Switching to branch gh-pages...
 git fetch
@@ -9,8 +14,8 @@ git checkout gh-pages
 git pull
 
 echo Adding files...
-mkdir -p $TRAVIS_BANCH/$TRAVIS_COMMIT
-mv -v ./preview/* ./$TRAVIS_BANCH/$TRAVIS_COMMIT/
+mkdir -p $TRAVIS_BRANCH/$TRAVIS_COMMIT
+mv -v ../preview/* ./$TRAVIS_BRANCH/$TRAVIS_COMMIT/
 
 echo <script>window.location+="/${TRAVIS_BRANCH}/${TRAVIS_COMMIT}"</script> > index.html
 
@@ -21,3 +26,4 @@ git push
 git checkout master
 
 echo Done!
+cd ..
