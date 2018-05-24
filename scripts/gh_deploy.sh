dc53f8c7fca89caa4e145cca6831cecb8a75bd81
lukas2005 <lukas2005.38@gmail.com>
2018-05-24 07:27:07 +0200

    test

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -1,15 +1,20 @@
 set -e
 
+echo Switching to branch gh-pages...
 git fetch
 git checkout gh-pages
 git pull
 
+echo Adding files...
 mkdir -p $TRAVIS_BANCH/$TRAVIS_COMMIT
 mv -v ./preview/* ./$TRAVIS_BANCH/$TRAVIS_COMMIT/
 
-echo "<script>window.location+='/${TRAVIS_BRANCH}/${TRAVIS_COMMIT}'</script>" > index.html
+echo <script>window.location+="/${TRAVIS_BRANCH}/${TRAVIS_COMMIT}"</script> > index.html
 
+echo Uploading...
 git add .
-git commit -a -m "Built and uploaded preview for commit $TRAVIS_COMMIT on branch $TRAVIS_BRANCH"
+git commit -a -m "Built and uploaded preview for commit $TRAVIS_BRANCH/$TRAVIS_COMMIT"
 git push
 git checkout master
+
+echo Done!
