1f554024716bb3db98868f766517dab7b13da821
lukas2005 <lukas2005.38@gmail.com>
2018-05-24 14:58:28 +0200

    changed to $TRAVIS_EVENT_TYPE for checking if its a pull request

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -20,20 +20,20 @@ git pull
 
 echo Adding files...
 
-if $TRAVIS_PULL_REQUEST == "false"; then
+if $TRAVIS_EVENT_TYPE == "push"; then
 	mkdir -p $TRAVIS_BRANCH/$TRAVIS_COMMIT
 	mv -v ../preview/* ./$TRAVIS_BRANCH/$TRAVIS_COMMIT/
 	echo "<script>window.location+='/${TRAVIS_BRANCH}/${TRAVIS_COMMIT}'</script>" > index.html
-else
+elif $TRAVIS_EVENT_TYPE == "pull_request"; then
 	mkdir -p ./PRs/$TRAVIS_PULL_REQUEST/$TRAVIS_COMMIT
 	mv -v ../preview/* ./PRs/$TRAVIS_PULL_REQUEST/$TRAVIS_COMMIT/
 fi
 
 echo Uploading...
 git add .
-if $TRAVIS_PULL_REQUEST == "false"; then
+if $TRAVIS_EVENT_TYPE == "push"; then
 	git commit -a -m "Built and uploaded preview for commit $TRAVIS_BRANCH/$TRAVIS_COMMIT"
-else
+elif $TRAVIS_EVENT_TYPE == "pull_request"; then
 	git commit -a -m "Built and uploaded preview for pull request from $TRAVIS_PULL_REQUEST_SLUG:$TRAVIS_PULL_REQUEST_BRANCH - commit: $TRAVIS_COMMIT"
 fi
 git push
