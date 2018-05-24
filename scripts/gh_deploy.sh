0da3d29f6ead2c586d553bae6d781ceabe386388
lukas2005 <lukas2005.38@gmail.com>
2018-05-24 07:12:07 +0200

    added index.html and `set -e` to gh-pages deploy

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -1,9 +1,13 @@
-git checkout gh-pages
+set -e
+
 git fetch
+git checkout gh-pages
 git pull
 
 mkdir -p $TRAVIS_BANCH/$TRAVIS_COMMIT
 mv -v ./preview/* ./$TRAVIS_BANCH/$TRAVIS_COMMIT/
+
+echo "<script>window.location+='/${TRAVIS_BRANCH}/${TRAVIS_COMMIT}'</script>" > index.html
 
 git add .
 git commit -a -m "Built and uploaded preview for commit $TRAVIS_COMMIT on branch $TRAVIS_BRANCH"
