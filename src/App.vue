6130c616ac76e2ac2bd13cad92bd0afd27c5d9ce
Łukasz Mariański <lmarianski@protonmail.com>
2020-06-05 00:42:33 +0200

    Huh

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,79 @@
-<template>
-	<div id="app">
-		<div id="nav">
-			<router-link to="/">
-				Home
-			</router-link> |
-			<router-link to="/about">
-				About
-			</router-link>
-		</div>
+<template web>
+	<div class="w-page">
+		<!-- <login></login> -->
+		<!-- <div class="row h-100">
+			<server-scroller class="col-fixed-70 h-100"></server-scroller>
+			<channel-list class="col-fixed-240 h-100"></channel-list>
+			<div class="row col-md-12 no-gutters h-100 offset-310">
+			</div>
+		</div>-->
 		<router-view />
 	</div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<template native></template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+import ServerScroller from 'components/ServerScroller.vue';
+import ChannelList from 'components/ChannelList.vue';
+
+const { VUE_APP_MODE } = process.env;
+
+@Component({
+	name: 'app',
+	components: {
+		ServerScroller,
+		ChannelList
+	}
+})
+export default class App extends Vue {
+	mounted () {
+		if (sessionStorage) {
+			const login = sessionStorage.getItem('login');
+			const password = sessionStorage.getItem('password');
+
+			if (login && password) {
+				(async () => {
+					await this.$store.dispatch('login', {
+						login,
+						password
+					});
+
+					this.$store.commit('CHANGE_STATE', 'USERSPACE');
+
+					this.$parent.$emit('apiReady', this.$store.state.api);
+
+					if (this.$route.path === '/login') this.goTo('/@me');
+				})();
+			}
+		}
+	}
+
+	public goTo (route) {
+		VUE_APP_MODE === 'web'
+			? this.$router.push(route)
+			: Vue.prototype.$navigator.navigate(route);
+	}
 }
+</script>
 
-#nav {
-  padding: 30px;
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style web lang="scss">
+@import "~styles/vars";
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.w-page {
+	height: 100%;
+	width: 100%;
+}
+</style>
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style native lang="scss">
+@import "~styles/vars";
+
+.w-page {
+	height: 100%;
+	width: 100%;
 }
 </style>
